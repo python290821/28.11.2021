0a4eb334390b7080827528b6a70b974c0753e912
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-28 18:42:13 +0200

    Update program.py

--- program.py
+++ program.py
@@ -2,5 +2,8 @@ import sqlite3
 from Customer import Customer
 from CustomerDataAccess import CustomerDataAccess
 
-dao = CustomerDataAccess('mydb.db')
-dao.print_all_customers();
+# dao = CustomerDataAccess('mydb.db')
+# dao.print_all_customers();
+
+print('Please enter you choice:')
+# now do it ...
